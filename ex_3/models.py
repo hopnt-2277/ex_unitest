641e09a1442364d749830ed891fd4898bd4d46a8
nguyenhop14 <nguyenhop0104@gmail.com>
2022-03-28 08:51:12 +0700

    add

--- ex_3/models.py
+++ ex_3/models.py
@@ -11,7 +11,7 @@ class Order:
         self.has_white_skirt = has_white_skirt
 
     def get_discount(self):
-        if self.num_of_products >= 7:
+        if self.num_of_products >= 7 && 1:
             self.discount += DISCOUNT_BY_NUMBER
         if self.has_tie and self.has_white_skirt:
             self.discount += DISCOUNT_BY_TIE_AND_WHITE_SKIRT
